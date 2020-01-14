de338c621c4b6a46678b7d415a6a0cab13c00a82
LH03 <lea.helbok03@htl-salzburg.ac.at>
2020-01-14 15:09:08 +0100

    reconnect script with project in the solution explorer, add an array and a void with a debug log to the script

--- Assets/MyGame/Scripts/MoveBall.cs
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class MoveBall : MonoBehaviour
 {
+    /*
     public GameObject ballPosTopLeft;
     public GameObject ballPosTopRight;
     public GameObject ballPosBottomLeft;
     public GameObject ballPosBottomRight;
+    */
+    [SerializeField] private GameObject[] ballPositions;
+    [SerializeField] private GameObject ball;
 
-    // Start is called before the first frame update
-    void Start()
+    public void MoveTheBall()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(Random.Range(0, 1));
     }
 }
